57c9e4b3abcd5377531cee1f601d9abb3601c06d
Martin Löfgren <martin.c.lofgren@gmail.com>
2019-05-29 08:22:29 +0200

    Update README, fix library functioning

---
+++ simulation/slblocks.m
@@ -0,0 +1,7 @@
+function blkStruct = slblocks
+		% This function specifies that the library should appear
+		% in the Library Browser
+		% and be cached in the browser repository
+		Browser.Library = 'follow_the_leader_library';
+		Browser.Name = 'Follow the leader';
+		blkStruct.Browser = Browser; 
